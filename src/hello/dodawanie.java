0950beadfac2370b3b15aee7e472470babbe1564
kurs <kurs@kurs-Komputer>
2017-08-20 10:37:39 +0200

    Dodałem dwie klasy - dodawanie i bye

---
+++ src/hello/dodawanie.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class dodawanie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=2+3;
+
+System.out.println("Wynik: " + a);
+	}
+
+}
